9a7f3dc8fe1605494af2f3e29cd8bc2eaed27372
c6wangya <wangyaolong9611@gmail.com>
2020-05-02 21:48:26 +0800

    fix bug in diff round test

--- examples/modules_test.py
+++ examples/modules_test.py
@@ -75,7 +75,8 @@ class ModuleTest(tf.test.TestCase):
             expected_output = [1, 2, 6, 2]
             expected_gradients = np.identity(4)
             output_tensor = m.differentiable_round(input_tensor)
-            grad_computed = tf.test.compute_gradient(input_tensor, (4,), output_tensor, (4,))
+            grad_computed = tf.test.compute_gradient(m.differentiable_round, input_tensor)
+            # grad_computed = tf.test.compute_gradient(input_tensor, (4,), output_tensor, (4,))
             self.assertAllClose(output_tensor, expected_output, atol=1e-3)
             self.assertAllClose(grad_computed, expected_gradients, atol=1e-3)
 
